b256354eb4947f35cafd8c15edf7564d9ecbbd80
Jake Hebert <jacobshebert@gmail.com>
2020-06-03 15:18:26 -0500

    add crossOrigin to gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        crossOrigin: "",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
